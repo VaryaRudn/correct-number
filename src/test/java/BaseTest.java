16d3c4b6a89f77ed1cd57bc64c03986ef126980f
Varya <vararudneva@gmail.com>
2025-05-06 12:19:24 +0300

    инициализация браузера

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+// ...
+
 public class BaseTest {
     @BeforeEach
     public void setUp() {
         // Открываем нужный сайт
+        WebDriverManager.chromedriver().setup();
         Selenide.open("https://severstal.com/rus/search/?search=%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F");
     }
 
